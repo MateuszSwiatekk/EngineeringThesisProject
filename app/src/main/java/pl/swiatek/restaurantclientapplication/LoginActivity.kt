1dd7ed01923b8500cc3dcba31d251cf3706b2e98
Mateusz Świątek <252858@student.pwr.edu.pl>
2022-10-13 16:45:31 +0200

    Add splash screen, basic register UI

---
+++ app/src/main/java/pl/swiatek/restaurantclientapplication/LoginActivity.kt
@@ -0,0 +1,19 @@
+package pl.swiatek.restaurantclientapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun registerClick(view: View){
+    val intent= Intent(this,RegisterActivity::class.java)
+    startActivity(intent)
+    }
+
+}
--- app/src/main/java/pl/swiatek/restaurantclientapplication/MainActivity.kt
+++ app/src/main/java/pl/swiatek/restaurantclientapplication/MainActivity.kt
@@ -1,11 +1,30 @@
 package pl.swiatek.restaurantclientapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.concurrent.timerTask
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startSecondActivity()
+    }
+    private fun startSecondActivity(){
+        if(!isDestroyed) {
+            val intent = Intent(this, LoginActivity::class.java)
+            val timTask= timerTask {
+                if(!isDestroyed){
+                    startActivity(intent)
+                    finish()
+                }
+            }
+            val timer=Timer()
+            timer.schedule(timTask,1500)
+        }
     }
 }
