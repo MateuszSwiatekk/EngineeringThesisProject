9a44469e79ac204e605b374d78f16b16e0d37133
Mateusz Świątek <252858@student.pwr.edu.pl>
2022-11-03 01:33:34 +0100

    Fixed booking bug

--- app/src/main/java/pl/swiatek/restaurantclientapplication/ChooseDateActivity.kt
+++ app/src/main/java/pl/swiatek/restaurantclientapplication/ChooseDateActivity.kt
@@ -3,92 +3,118 @@ package pl.swiatek.restaurantclientapplication
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.DatePicker
 import android.widget.TextView
 import android.widget.TimePicker
 import android.widget.Toast
-import java.text.SimpleDateFormat
+import androidx.appcompat.app.AppCompatActivity
 import java.time.LocalDateTime
-import java.time.LocalDateTime.*
-import java.time.format.DateTimeFormatter
+import java.time.LocalDateTime.now
 import java.util.*
 
-class ChooseDateActivity : AppCompatActivity(),DatePickerDialog.OnDateSetListener,TimePickerDialog.OnTimeSetListener {
+class ChooseDateActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
+    TimePickerDialog.OnTimeSetListener {
 
-    var day=0;
-    var month=0;
-    var year=0;
-    var hour =0;
-    var minute=0;
+    var day = 0
+    var month = 0
+    var year = 0
+    var hour = 0
+    var minute = 0
 
-    var savedDay=0;
-    var savedMonth=0;
-    var savedYear=0;
-    var savedHour =0;
-    var savedMinute=0;
+    var savedDay = 0
+    var savedMonth = 0
+    var savedYear = 0
+    var savedHour = 0
+    var savedMinute = 0
 
-    private lateinit var dateText:TextView
-    private lateinit var currentDate:LocalDateTime
-    private lateinit var datePicked:LocalDateTime
+    private lateinit var dateText: TextView
+    private lateinit var currentDate: LocalDateTime
+    private lateinit var datePicked: LocalDateTime
+    private lateinit var savedMonth2: String
+    private lateinit var savedDay2: String
+    private lateinit var savedHour2: String
+    private lateinit var savedMinute2: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_date)
-        dateText=findViewById(R.id.dateText)
+        dateText = findViewById(R.id.dateText)
         currentDate = now()
 
     }
 
-    private fun getDateTime(){
-        val cal=Calendar.getInstance()
-        day=cal.get(Calendar.DAY_OF_MONTH)
-        month=cal.get(Calendar.MONTH)
-        year=cal.get(Calendar.YEAR)
-        hour=cal.get(Calendar.HOUR_OF_DAY)
-        minute=cal.get(Calendar.MINUTE)
+    private fun getDateTime() {
+        val cal = Calendar.getInstance()
+        day = cal.get(Calendar.DAY_OF_MONTH)
+        month = cal.get(Calendar.MONTH)
+        year = cal.get(Calendar.YEAR)
+        hour = cal.get(Calendar.HOUR_OF_DAY)
+        minute = cal.get(Calendar.MINUTE)
     }
 
-    fun pickDateClick(view:View){
+    fun pickDateClick(view: View) {
         getDateTime()
-        DatePickerDialog(this,this,year,month,day).show()
+        DatePickerDialog(this, this, year, month, day).show()
     }
 
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-        savedDay=dayOfMonth
-        savedMonth=month+1
-        savedYear=year
+        savedDay = dayOfMonth
+        savedMonth = month + 1
+        savedYear = year
 
         getDateTime()
-        TimePickerDialog(this,this,hour,minute,true).show()
+        TimePickerDialog(this, this, hour, minute, true).show()
     }
 
     override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
-        savedHour=hourOfDay
-        savedMinute=minute
-        dateText.text="$savedDay-$savedMonth-$savedYear, $savedHour:$savedMinute"
-        datePicked=of(savedYear,savedMonth,savedDay,savedHour,savedMinute)
+        savedHour = hourOfDay
+        savedMinute = minute
+        dateText.text = "$savedDay-$savedMonth-$savedYear, $savedHour:$savedMinute"
+        datePicked = LocalDateTime.of(savedYear, savedMonth, savedDay, savedHour, savedMinute)
     }
 
-    fun onBookTableClick(view: View){
-        if(savedYear==0){
-            Toast.makeText(applicationContext,"Pick date!",Toast.LENGTH_SHORT).show()
+    fun onBookTableClick(view: View) {
+        if (savedYear == 0) {
+            Toast.makeText(applicationContext, "Pick date!", Toast.LENGTH_SHORT).show()
             return
         }
 
-            if(datePicked.isBefore(currentDate)){
-                Toast.makeText(applicationContext,"Invalid date!",Toast.LENGTH_SHORT).show()
-                return
-            }
-
-            val intent = Intent(this, ChooseTableActivity::class.java)
-            intent.putExtra(
-                "selectedDate",
-                arrayOf(savedYear.toString(), savedMonth.toString(), savedDay.toString(), savedHour.toString(), savedMinute.toString())
-            )
-            startActivity(intent)
+        if (datePicked.isBefore(currentDate)) {
+            Toast.makeText(applicationContext, "Invalid date!", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (savedHour<12||savedHour>21) {
+            Toast.makeText(applicationContext, "Sorry, we're closed!", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val intent = Intent(this, ChooseTableActivity::class.java)
+        if (savedMonth < 10) {
+            savedMonth2 = "0$savedMonth"
+        } else {
+            savedMonth2 = savedMonth.toString()
+        }
+        if (savedDay < 10) {
+            savedDay2 = "0$savedDay"
+        } else {
+            savedDay2 = savedDay.toString()
+        }
+        if (savedHour < 10) {
+            savedHour2 = "0$savedHour"
+        } else {
+            savedHour2 = savedHour.toString()
+        }
+        if (savedMinute < 10) {
+            savedMinute2 = "0$savedMinute"
+        } else {
+            savedMinute2 = savedMinute.toString()
+        }
+        intent.putExtra(
+            "selectedDate",
+            arrayOf(savedYear.toString(), savedMonth2, savedDay2, savedHour2, savedMinute2)
+        )
+        startActivity(intent)
 
     }
 
--- app/src/main/java/pl/swiatek/restaurantclientapplication/ChooseTableActivity.kt
+++ app/src/main/java/pl/swiatek/restaurantclientapplication/ChooseTableActivity.kt
@@ -15,12 +15,12 @@ import java.time.format.DateTimeFormatter
 import kotlin.properties.Delegates
 
 class ChooseTableActivity : AppCompatActivity() {
-    private lateinit var email:String
-    private lateinit var userId:String
-    private lateinit var data2:String
-    private lateinit var data3:String
-    private lateinit var bookedTablesData:DatabaseReference
-    private lateinit var bookedTables:MutableSet<String>
+    private lateinit var email: String
+    private lateinit var userId: String
+    private lateinit var data2: String
+    private lateinit var data3: String
+    private lateinit var bookedTablesData: DatabaseReference
+    private lateinit var bookedTables: MutableSet<String>
     private var bookedAmount by Delegates.notNull<Long>()
     private lateinit var formatter: DateTimeFormatter
 
@@ -29,42 +29,56 @@ class ChooseTableActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_table)
 
-        val table1Btn=findViewById<Button>(R.id.table1)
-        val table2Btn=findViewById<Button>(R.id.table2)
-        val table3Btn=findViewById<Button>(R.id.table3)
-        val table4Btn=findViewById<Button>(R.id.table4)
-        val table5Btn=findViewById<Button>(R.id.table5)
-        val table6Btn=findViewById<Button>(R.id.table6)
-        val table7Btn=findViewById<Button>(R.id.table7)
-        val table8Btn=findViewById<Button>(R.id.table8)
-        val table9Btn=findViewById<Button>(R.id.table9)
-        val table10Btn=findViewById<Button>(R.id.table10)
+        val table1Btn = findViewById<Button>(R.id.table1)
+        val table2Btn = findViewById<Button>(R.id.table2)
+        val table3Btn = findViewById<Button>(R.id.table3)
+        val table4Btn = findViewById<Button>(R.id.table4)
+        val table5Btn = findViewById<Button>(R.id.table5)
+        val table6Btn = findViewById<Button>(R.id.table6)
+        val table7Btn = findViewById<Button>(R.id.table7)
+        val table8Btn = findViewById<Button>(R.id.table8)
+        val table9Btn = findViewById<Button>(R.id.table9)
+        val table10Btn = findViewById<Button>(R.id.table10)
 
-        val arrayTableBtns= arrayOf(table1Btn,table2Btn,table3Btn,table4Btn,table5Btn,table6Btn,table7Btn,table8Btn,table9Btn,table10Btn)
+        val arrayTableBtns = arrayOf(
+            table1Btn,
+            table2Btn,
+            table3Btn,
+            table4Btn,
+            table5Btn,
+            table6Btn,
+            table7Btn,
+            table8Btn,
+            table9Btn,
+            table10Btn
+        )
 
-        val data=intent.getStringArrayExtra("selectedDate")
-        data2 = (data!![0]).toString()+"-"+(data[1]).toString()+"-"+(data[2]).toString()+" "+(data[3]).toString()+":"+(data[4]).toString()
-        data3 = (data[0]).toString()+"-"+(data[1]).toString()+"-"+(data[2]).toString()+" "+(data[3].toInt()+1).toString()+":"+(data[4].toInt()).toString()
+        val data = intent.getStringArrayExtra("selectedDate")
+        data2 =
+            (data!![0]).toString() + "-" + (data[1]).toString() + "-" + (data[2]).toString() + " " + (data[3]).toString() + ":" + (data[4]).toString()
+        data3 =
+            (data[0]).toString() + "-" + (data[1]).toString() + "-" + (data[2]).toString() + " " + (data[3].toInt() + 1).toString() + ":" + (data[4].toInt()).toString()
 
-        userId= FirebaseAuth.getInstance().currentUser!!.uid
-        email= FirebaseAuth.getInstance().currentUser!!.email.toString()
-        bookedTablesData=FirebaseDatabase.getInstance().getReference("BookedTables")
-        bookedTables= mutableSetOf()
+        userId = FirebaseAuth.getInstance().currentUser!!.uid
+        email = FirebaseAuth.getInstance().currentUser!!.email.toString()
+        bookedTablesData = FirebaseDatabase.getInstance().getReference("BookedTables")
+        bookedTables = mutableSetOf()
 
-        val queryGetBookedTables = bookedTablesData.orderByChild("endDate").startAt(data2).endAt(data3)
-        queryGetBookedTables.addValueEventListener(object:ValueEventListener{
+        val queryGetBookedTables =
+            bookedTablesData.orderByChild("endDate").startAt(data2).endAt(data3)
+        queryGetBookedTables.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 bookedTables.clear()
-                if(snapshot.hasChildren()){
-                    for(snapData in snapshot.children){
-                        val tableData=snapData.getValue(BookedTable::class.java)
+                if (snapshot.hasChildren()) {
+                    for (snapData in snapshot.children) {
+                        val tableData = snapData.getValue(BookedTable::class.java)
                         bookedTables.add(tableData!!.tableNumber)
                     }
-                    for(i in 0..9){
-                        if (bookedTables.contains((i+1).toString())) {
+                    for (i in 0..9) {
+                        if (bookedTables.contains((i + 1).toString())) {
                             arrayTableBtns[i].setBackgroundColor(Color.GRAY)
                             arrayTableBtns[i].isClickable = false
-                        }else{
+                        } else {
                             arrayTableBtns[i].setBackgroundColor(Color.parseColor("#3700B3"))
                             arrayTableBtns[i].isClickable = true
                         }
@@ -79,19 +93,21 @@ class ChooseTableActivity : AppCompatActivity() {
         })
 
         val queryAmountBooked = bookedTablesData.orderByChild("email").equalTo(email)
-        queryAmountBooked.addValueEventListener(object :ValueEventListener{
+        queryAmountBooked.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                bookedAmount=0
-                if(snapshot.hasChildren()){
-                    val now =now()
-                    for(snapData in snapshot.children){
+                bookedAmount = 0
+                if (snapshot.hasChildren()) {
+                    val x = now()
+
+                    for (snapData in snapshot.children) {
                         val child = snapData.getValue(BookedTable::class.java)
-                        if(LocalDateTime.parse(child!!.endDate,formatter).isAfter(now)){
+                        val y = LocalDateTime.parse(child!!.getEndDate(), formatter)
+                        if (y.isAfter(x)) {
                             bookedAmount += 1
                         }
                     }
-                }else{
-                    bookedAmount=0
+                } else {
+                    bookedAmount = 0
                 }
             }
 
@@ -102,93 +118,112 @@ class ChooseTableActivity : AppCompatActivity() {
         })
     }
 
-    fun bookTable(tableNum:String){
+    fun bookTable(tableNum: String) {
         val builder = AlertDialog.Builder(this)
         builder.setMessage("Do you really want to book this table?")
         builder.setTitle("Confirmation")
         builder.setCancelable(false)
 
-        builder.setPositiveButton("Yes") {
-                dialog, which -> val booking=BookedTable(tableNum,data2,data3,email)
+        builder.setPositiveButton("Yes") { dialog, which ->
+            val booking = BookedTable(tableNum, data2, data3, email)
             bookedTablesData.child(bookedTablesData.push().key!!)
                 .setValue(booking)
         }
 
-        builder.setNegativeButton("No") {
-                dialog, which -> dialog.cancel()
+        builder.setNegativeButton("No") { dialog, which ->
+            dialog.cancel()
         }
         val alertDialog = builder.create()
         alertDialog.show()
     }
 
-    fun table1Click(view: View){
-        if(bookedAmount<3) {
+    fun table1Click(view: View) {
+        if (bookedAmount < 3) {
             bookTable("1")
-        }else{
-            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT)
+                .show()
         }
     }
-    fun table2Click(view: View){
-        if(bookedAmount<3) {
+
+    fun table2Click(view: View) {
+        if (bookedAmount < 3) {
             bookTable("2")
-        }else{
-            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT)
+                .show()
         }
     }
-    fun table3Click(view: View){
-        if(bookedAmount<3) {
+
+    fun table3Click(view: View) {
+        if (bookedAmount < 3) {
             bookTable("3")
-        }else{
-            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT)
+                .show()
         }
     }
-    fun table4Click(view: View){
-        if(bookedAmount<3) {
+
+    fun table4Click(view: View) {
+        if (bookedAmount < 3) {
             bookTable("4")
-        }else{
-            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT)
+                .show()
         }
     }
-    fun table5Click(view: View){
-        if(bookedAmount<3) {
+
+    fun table5Click(view: View) {
+        if (bookedAmount < 3) {
             bookTable("5")
-        }else{
-            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT)
+                .show()
         }
     }
-    fun table6Click(view: View){
-        if(bookedAmount<3) {
+
+    fun table6Click(view: View) {
+        if (bookedAmount < 3) {
             bookTable("6")
-        }else{
-            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT)
+                .show()
         }
     }
-    fun table7Click(view: View){
-        if(bookedAmount<3) {
+
+    fun table7Click(view: View) {
+        if (bookedAmount < 3) {
             bookTable("7")
-        }else{
-            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT)
+                .show()
         }
     }
-    fun table8Click(view: View){
-        if(bookedAmount<3) {
+
+    fun table8Click(view: View) {
+        if (bookedAmount < 3) {
             bookTable("8")
-        }else{
-            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT)
+                .show()
         }
     }
-    fun table9Click(view: View){
-        if(bookedAmount<3) {
+
+    fun table9Click(view: View) {
+        if (bookedAmount < 3) {
             bookTable("9")
-        }else{
-            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT)
+                .show()
         }
     }
-    fun table10Click(view: View){
-        if(bookedAmount<3) {
+
+    fun table10Click(view: View) {
+        if (bookedAmount < 3) {
             bookTable("10")
-        }else{
-            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "Can't make more reservations!", Toast.LENGTH_SHORT)
+                .show()
         }
     }
 }
